db54c515bc7319a0ff64fc1efcf4a9f519e5a0e6
kyeiiih <jaydevsengineer@gmail.com>
2026-05-25 12:39:12 +0300

    Feat: add masking dependency

--- src/main/java/com/jdevs/securebankapi/auth/controller/AuthController.java
+++ src/main/java/com/jdevs/securebankapi/auth/controller/AuthController.java
@@ -6,21 +6,26 @@ import com.jdevs.securebankapi.auth.dto.RegisterRequest;
 import com.jdevs.securebankapi.auth.dto.RegisterResponse;
 import com.jdevs.securebankapi.auth.service.AuthService;
 import com.jdevs.securebankapi.shared.response.ApiResponse;
+import com.mullatoez.security.logger.spring.MullatoezSensitiveLogger;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
     private final AuthService authService;
+    private final MullatoezSensitiveLogger sensitiveLogger;
 
     public AuthController(
-            AuthService authService
+            AuthService authService, MullatoezSensitiveLogger sensitiveLogger
     ) {
         this.authService = authService;
+        this.sensitiveLogger = sensitiveLogger;
     }
 
     @PostMapping("/register")
@@ -38,6 +43,7 @@ public class AuthController {
     public ResponseEntity<ApiResponse<LoginResponse>> login(
             @Valid @RequestBody LoginRequest request
     ) {
+        log.info("Incoming login request={}", sensitiveLogger.mask(request));
         LoginResponse response = authService.login(request);
 
         return ResponseEntity.ok(
--- src/main/java/com/jdevs/securebankapi/auth/dto/LoginRequest.java
+++ src/main/java/com/jdevs/securebankapi/auth/dto/LoginRequest.java
@@ -1,14 +1,18 @@
 package com.jdevs.securebankapi.auth.dto;
 
+import com.mullatoez.security.logger.core.Sensitive;
+import com.mullatoez.security.logger.core.SensitiveMode;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
 
+        @Sensitive(mode = SensitiveMode.PARTIAL)
         @NotBlank(message = "Email is required")
         @Email(message = "Email must be valid")
         String email,
 
+        @Sensitive(mode = SensitiveMode.FULL)
         @NotBlank(message = "Password is required")
         String password
 ) {
